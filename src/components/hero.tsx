05bd2d3c4755be1a89dee7465f04404a1deccc3b
Michael Bell <michael@thehybridminds.com>
2023-02-09 12:37:27 +0800

    Hero fix

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -114,7 +114,7 @@ export const Hero = () => {
             </div>
           </Dialog.Panel>
         </Dialog>
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56 h-screen">
+        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-72 ">
           {/* <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full py-1 px-3 text-sm leading-6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20">
               Announcing our next round of funding.{' '}
