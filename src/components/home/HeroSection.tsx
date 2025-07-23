99ce3b2423212382b251ac7ad8ab4bef0fb41a0c
Oneeb Faisal <oneebfaisal@gmail.com>
2025-07-23 20:50:11 +0500

    Update HeroSection.tsx

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -3,7 +3,7 @@ import ButtonCTA from "../../commonComponents/ButtonCTA";
 
 export default function HeroSection() {
   return (
-    <div className="h-screen w-full bg-indigo-700 bg-home bg-cover bg-center">
+    <div className="h-screen w-full bg-home bg-cover bg-center">
       <div className="page-container flex h-full">
         <div className="flex h-full w-2/3 flex-col items-center justify-center">
           <h1 className="mb-8 w-full text-center text-primary">
